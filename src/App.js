986ed47a1ceafe922a2f026863fa986465d7acc8
derikbf <derikb@gmail.com>
2021-07-13 20:07:16 -0300

    desenhando a estrutura do perfil do usuario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="App">
-   github api
-    </div>
+   <main>
+    <Layout>
+      <div>
+        <img source="https://avatars.githubusercontent.com/u/39008435?v=4" alt="Avatar of user"/>
+        <h1>Dérik B. Favoretti</h1>
+        <h3>Username: </h3>
+        <span>derikbf</span>
+        <div>
+          <div>
+            <h4>Followers</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>5</span>
+          </div>
+        </div>
+      </div>
+      <div>Reporitories</div>
+      <div>Starreds</div>
+    </Layout>
+   </main>
   );
 }
 
