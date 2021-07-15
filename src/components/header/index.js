d76b2b11ae01b5efae92fbcc5eac7f3518ae6671
derikbf <derikb@gmail.com>
2021-07-14 22:17:08 -0300

    iniciando a insercao do campo de busca

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styled';
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <input type="text" />
+      <button> buscar </button>
+
+    </S.Wrapper>
+  );
+}
+
+export default Header
