ddd7101e7236826759268796a9e424abe0d55eb3
derikbf <derikb@gmail.com>
2021-07-15 19:51:37 -0300

    criando o busca

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import * as S from './styled';
+import userGithub from "../../hooks/github-hooks";
 
 const Header = () => {
+  const { getUser } = useGithub();
+  const [ usernameForSearch, setUsernameForSearch ] = useState()
+
+  const submitGetUser = () => {
+    if ( !usernameForSearch ) return;
+    return getUser( usernameForSearch );
+  }
+
   return (
     <S.Wrapper>
-      <input type="text" />
-      <button> buscar </button>
-
+      <input 
+        type="text"
+        placeholder="digite o username para pesquisa..."
+        onChange={submitGetUser}
+      />
+      <button type="submit" > 
+        <span>
+          Buscar
+        </span>
+      </button>
     </S.Wrapper>
   );
 }
--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -1,32 +1,54 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useCallback } from 'react';
+import api from '.../services/api';
 
 export const GithubContext = createContext({
   user: {},
   repositories: [],
   starred: [],
+  loading: false,
 });
 
-const GithubProvider = ({children}) => {
-  
-  const [ githubState, setGithubState ]  = useState({
-    user: {
-      login: undefined,
-      name: undefined,
-      html_url: undefined,
-      blog: undefined,
-      company: undefined,
-      location: undefined,
-      followers: 0,
-      following: 0,
-      public_gists: 0,
-    },
-    repositories: [],
-    starred: [],
-  })
-  
+  const GithubProvider = ({ children }) => {
+    const [githubState, setGithubState] = useState({
+      loading: false,
+      user: {
+        login: undefined,
+        name: undefined,
+        html_url: undefined,
+        blog: undefined,
+        company: undefined,
+        location: undefined,
+        followers: undefined,
+        following: undefined,
+        public_gists: undefined,
+      },
+      repositories: [],
+      starred: [],
+    });
+
+    const getUser = ( username ) => {
+      api.get(`users/${username}`).then(({ data: { user } }) => {
+        setGithubState((prevState) => ({
+          ...prevState,
+          user: {
+            login: user.login,
+            name: user.name,
+            html_url: user.html_url,
+            blog: user.blog,
+            company: user.company,
+            location: user.location,
+            followers: user.followers,
+            following: user.following,
+            public_gists: user.public_gists,
+          },
+        }));
+      });
+    };  
+
   const contextValue = {
     githubState,
-  }
+    getUser: useCallback((username) => getUser(username), []), 
+  };
   
   return (
     <GithubContext.Provider 
@@ -35,6 +57,6 @@ const GithubProvider = ({children}) => {
     {children}  
     </GithubContext.Provider>
   );
-}
+};
 
 export default GithubProvider;
